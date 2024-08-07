86c663d5a07a4b92f799b6873df828ef41c7ea80
mingu <mc12alsrn@gmail.com>
2024-08-07 11:28:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    const ticketBtn = document.getElementById('ticketBtn');
+    const donateBtn = document.getElementById('donateBtn');
+    const leftCurtain = document.querySelector('.curtain.left');
+    const rightCurtain = document.querySelector('.curtain.right');
+    const poster = document.querySelector('.poster');
+    
+    setInterval(() => {
+        poster.style.transform = 'rotateY(20deg)';
+        setTimeout(() => {
+            poster.style.transform = 'rotateY(380deg)';
+        }, 3000);
+    }, 4000);
+
+    // 커튼 애니메이션
+    leftCurtain.style.transform = 'translateX(0)';
+    rightCurtain.style.transform = 'translateX(0)';
+
+setTimeout(() => {
+    leftCurtain.style.transform = 'translateX(-100%)';
+    rightCurtain.style.transform = 'translateX(100%)';
+}, 500);
+
+
+    ticketBtn.addEventListener('click', () => {
+        leftCurtain.style.transform = 'translateX(0)';
+        rightCurtain.style.transform = 'translateX(0)';
+        setTimeout(() => {
+            window.location.href = 'https://uoukangmingu.github.io/precheck/';
+        }, 1000);
+    });
+
+    donateBtn.addEventListener('click', () => {
+        leftCurtain.style.transform = 'translateX(0)';
+        rightCurtain.style.transform = 'translateX(0)';
+        setTimeout(() => {
+            window.location.href = 'https://payapplite.com/l/asfCnZ';
+        }, 1000);
+    });
+});
